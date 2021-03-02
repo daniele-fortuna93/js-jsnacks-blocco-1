1aaaecfb5a38e3d8a60f540926258371df1f9a5f
Daniele <bambudio@libero.it>
2021-03-02 13:01:10 +0100

    Snack7 completo

--- snack-5/scripts.js
+++ snack-5/scripts.js
@@ -3,14 +3,17 @@
  // 1. Definisco array vuoto
  var dispari = [];
 
-var num;
  // 2. Riempio array con numeri dispari
+ var num;
+
  for (var i = 0; i < 6; i++) {
    num = parseInt(prompt("Inserisci un numero"));
+   // 3. Controllo valore inserito
    if ( !isNaN(num) ){
      if ( !((num % 2) == 0 )) {
        dispari.push(num);
      }
    }
  }
+ // 4. Stampo array
  console.log(dispari);
--- snack6/scripts.js
+++ snack6/scripts.js
@@ -2,13 +2,15 @@
 
  // 1. Chiedo N all'utente
  var n = parseInt(prompt("Inserisci un numero"));
+ // 2. Controllo il valore inserito
  if ( isNaN(n) ) {
    alert("Per favore inserisci un numero.");
    n = parseInt(prompt("Inserisci un numero"));
  }
 
-var x;
- // 2. Stampo i quadrati fino a Number
+ // 3. Stampo i quadrati fino a n
+ var x;
+ 
  if ( !isNaN(n)) {
    for (var i = 0; i < n; i++) {
      x = 1;
