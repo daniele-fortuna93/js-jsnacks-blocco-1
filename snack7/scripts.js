1a1d9493252729b15abeed07cbec6a365e98285d
Daniele <bambudio@libero.it>
2021-03-03 12:53:38 +0100

    Refactoring snack7

--- snack7/scripts.js
+++ snack7/scripts.js
@@ -1,10 +1,6 @@
  // Stampa le potenze di 2 fino a 1000.
 
- // 1. Stanzio una variabile n
- var n = 1;
- 
- // 2. Ciclo la variabile fino a n < 1000
- for (var i = 1; ( n * 2) < 1000; i++) {
-   n = n * 2;
+ // 1. Ciclo la variabile fino a n < 1000
+ for (var n = 1; n  < 1000; n = n * 2 ) {
    console.log(n);
  }
